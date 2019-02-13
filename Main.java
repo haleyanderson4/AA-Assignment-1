266b43454f7f0513ae49460b89807d8c2d168dbd
Jennifer Prosinski <prosi100@mail.chapman.edu>
2019-02-13 12:07:50 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,22 +5,58 @@
  */
 
 import java.util.*;
-
+import java.lang.String;
 
 public class Main
 {
 
     public static void main(String[] args)
     {
-        String fileName = args[0];
-        for () 
-        {
-            List<String> nfaStates = new ArrayList<String>;
-            List<String> language = new ArrayList<String>;
-            String startState = "";
-            Set<String> nfaAcceptS = new HashSet<String>;
-            List<String> nfaRules = new ArrayList<String>;
-            //parse
+        Scanner in = new Scanner(System.in); 
+        String fileName = in.args[0]; 
+        
+        File file = new File(fileName); 
+        Scanner sc = new Scanner(file); 
+        List<String> nfaStates = new ArrayList<String>;
+        List<String> language = new ArrayList<String>;
+        String startState = "";
+        Set<String> nfaAcceptS = new HashSet<String>;
+        List<String> nfaRules = new ArrayList<String>;
+        int lineCount = 1;
+        while (sc.hasNextLine()){
+            String line = sc.nextLine();
+            switch (lineCount){
+                case 1: lineCount = 1;
+                    for (character c in line){
+                        if (c != '\t'){
+                            nfaStates.add(c);
+                        }
+                    }
+                    lineCount++;
+                    break;
+                case 2: lineCount = 2;
+                    for (character c in line){
+                        if (c != '\t'){
+                            nfaStates.add(c);
+                        }
+                    }
+                    lineCount++;
+                    break;
+                case 3: lineCount = 3;
+                    startState = line;
+                    lineCount++;
+                    break;
+                case 4: lineCount = 4;
+                    for (character c in line){
+                        if (c != '\t'){
+                            nfaAcceptS.add(c);
+                        }
+                    }
+                    lineCount++;
+                    break;
+                default: 
+                    nfaRules.add(line);
+                    break; 
         }
         
         List<String> dfaStates = new ArrayList<String>;
