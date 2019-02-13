5574e4a3f6796cd65c9aa0b2d355e373611fa715
Haley Anderson <haleyanderson4@comcast.net>
2019-02-13 11:31:29 -0800

    Variables!

---
+++ Main.java
@@ -0,0 +1,30 @@
+/**
+ * @TODO
+ * Parsing
+ * Conversion
+ */
+
+import java.util.*;
+
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        String fileName = args[0];
+        for () 
+        {
+            List<String> nfaStates = new ArrayList<String>;
+            List<String> language = new ArrayList<String>;
+            String startState = "";
+            Set<String> nfaAcceptS = new HashSet<String>;
+            List<String> nfaRules = new ArrayList<String>;
+            //parse
+        }
+        
+        List<String> dfaStates = new ArrayList<String>;
+        List<String> dfaAcceptS = new ArrayList<String>;
+        List<String> dfaRules = new ArrayList<String>;
+    }
+}
