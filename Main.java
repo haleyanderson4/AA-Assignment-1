8776713ca4a0b98e0284c5587fe3dbb753ab813a
Jennifer Prosinski <prosi100@mail.chapman.edu>
2019-02-18 21:58:14 -0800

    fixed switch and eps close

--- Main.java
+++ Main.java
@@ -6,28 +6,33 @@
 
 import java.util.*;
 import java.lang.String;
+import java.io.*;
 
-public class Main
+public class Assignment1
 {
 
     public static void main(String[] args)
     {
         try
         {
-            //Scanner in = new Scanner(System.in); 
             File file = null;
-            if (0 < args.length) {
+            if (0 < args.length) 
+            {
                file = new File(args[0]);
-            } else {
+            } 
+            else 
+            {
                System.err.println("Invalid arguments count:" + args.length);
             } 
             Scanner sc = new Scanner(file); 
+            
             List<String> nfaStates = new ArrayList<String>();
             List<String> language = new ArrayList<String>();
             String startState = "";
             Set<String> nfaAcceptS = new HashSet<String>();
             List<String> nfaRules = new ArrayList<String>();
             List<String> nfaEplisonRules = new ArrayList<String>();
+            
             int lineCount = 1;
             while (sc.hasNextLine())  //loop through all the lines in text file
             {  
@@ -50,7 +55,7 @@ public class Main
                             char c = line.charAt(i);
                             if (c != '\t')
                             {
-                                nfaStates.add(""+c);
+                                language.add(""+c);
                             }
                         }
                         lineCount++;
@@ -77,52 +82,52 @@ public class Main
                             nfaRules.add(line);
                         break; 
                 }
+            }
+            List<String> dfaStates = new ArrayList<String>();
+            List<String> dfaAcceptS = new ArrayList<String>();
+            List<String> dfaRules = new ArrayList<String>();
+            //variable declarations
+
+            List<String> eplisonClosure = new ArrayList<String>(); //list of new eplison closure states
+            epsClosureCreate(eplisonClosure, nfaEplisonRules, nfaStates); //method to populate eplison states
+
+            runThrough(startState, nfaRules, dfaRules, dfaStates);
+            //calling the recursive method to create the dfaRules, begins with start state
+
+            // write DFA to new text file
+            FileWriter outFile = new FileWriter("DFAinformation.txt");
+            for(String str:dfaStates)
+            {
+                outFile.write(str);
+            }
+            outFile.write("\n");
+            for(String str:language)
+            {
+                outFile.write(str);
+            }
+            outFile.write("\n");
+            outFile.write(startState);
+            outFile.write("\n");
+            for(String str:dfaAcceptS)
+            {
+                outFile.write(str);
+            }
+            outFile.write("\n");
+            for(String str:dfaRules)
+            {
+                outFile.write(str);
+            }
+            outFile.close();
+            }
+            catch(Exception e)
+            {
+                System.err.println("Invalid input");
+            }
         }
-        List<String> dfaStates = new ArrayList<String>();
-        List<String> dfaAcceptS = new ArrayList<String>();
-        List<String> dfaRules = new ArrayList<String>();
-        //variable declarations
-        
-        List<String> eplisonClosure = new ArrayList<String>(); //list of new eplison closure states
-        epsClosureCreate(eplisonClosure, nfaEplisonRules, nfaStates); //method to populate eplison states
-        
-        runThrough(startState, nfaRules, dfaRules, dfaStates);
-        //calling the recursive method to create the dfaRules, begins with start state
-        
-        // write DFA to new text file
-        FileWriter outFile = new FileWriter("DFAinformation.txt");
-        for(String str:dfaStates)
-        {
-            outFile.write(str);
-        }
-        outFile.write("\n");
-        for(String str:language)
-        {
-            outFile.write(str);
-        }
-        outFile.write("\n");
-        outFile.write(startState);
-        outFile.write("\n");
-        for(String str:dfaAcceptS)
-        {
-            outFile.write(str);
-        }
-        outFile.write("\n");
-        for(String str:dfaRules)
-        {
-            outFile.write(str);
-        }
-        outFile.close();
-        }
-        catch(Exception e)
-        {
-            System.err.println("Invalid input");
-        }
-    }
-    
+
     public static List<String> epsClosureCreate(List<String> eplisonClosure, List<String> nfaEplisonRules, List<String> nfaStates)
     {
-        for(int checkState = 0; checkState < nfaStates; checkState++) //looping through all states in original NFA
+        for(int checkState = 0; checkState < nfaStates.size(); checkState++) //looping through all states in original NFA
         {
             String newState = "{";
             for(int checkRule = 0; checkRule < nfaEplisonRules.size(); checkRule++) //looping through all of the rules that have eplisons
@@ -131,7 +136,7 @@ public class Main
                 newState = newState + ruleState; //creating the grouped state
                 if(ruleState == nfaStates.get(checkState)) // if the state mentioned is equal to the original state we are looking at, we move to combine them
                  {
-                    String destinationState = nfaEplisonRules.get(checkRule).substring(6));
+                    String destinationState = nfaEplisonRules.get(checkRule).substring(6);
                     newState = newState + "," + destinationState; //this is creating the grouped state
                  } //do this for all rules
             }
@@ -142,6 +147,7 @@ public class Main
         
         for(int alreadyClosed = 0; alreadyClosed < eplisonClosure.size(); alreadyClosed++) //now were checking that all epsilon closures are complete
         {
+            String eplisonState = "";
             String currentState = eplisonClosure.get(alreadyClosed); //the state set
             for(int comparisonClose = 0; comparisonClose < eplisonClosure.size(); comparisonClose++) //now were checking that all epsilon closures are complete
             {
@@ -149,18 +155,19 @@ public class Main
                 {
                     continue; //don't want to compare a rule to itself
                 }
-                String stateRule = eplisonClosure.get(comparisonState);
+                String stateRule = eplisonClosure.get(comparisonClose);
                 for(int stateChars = 1; stateChars < stateRule.length(); stateChars++) //go through all of the states mentioned in the epsilon closure
                 {
-                    String eplisonState = stateRule.substring(stateChars,stateChars+1); //to skip the { and look at the first state
+                    eplisonState = stateRule.substring(stateChars,stateChars+1); //to skip the { and look at the first state
                     if(eplisonState == currentState) //if they match
                     {
                         eplisonState = eplisonState.substring(0,-1) + "," + eplisonState.substring(3); // add the cascading effect to the state we are looking at
                     }
                 }
             }
-            eplisonClosure.edit(alreadyClosed, eplisonState); //now a full epsilon closure for sure!! wohoo!!
+            eplisonClosure.set(alreadyClosed, eplisonState); //now a full epsilon closure for sure!! wohoo!!
         } // do this until we know it doesnt eplison to anything else
+        return eplisonClosure;
     }
     
     public static String runThrough(String currentState, List<String> nfaRules, List<String> dfaRules, List<String> dfaStates)
